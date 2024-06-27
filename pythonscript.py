89497c4a03af3412397f717cdafea7334c32bd14
Zion44719 <zionctech@gmail.com>
2024-06-27 06:31:15 -0700

    first commit

---
+++ pythonscript.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+myName = "My Name is Oluwafemi"
+print(myName.upper())
+print(myName.lower())
+print(myName.title())
+print(len(myName))
+
+#Computer Accessories
+#Addition
+mouse = 1000
+keyboard = 5000
+print(mouse + keyboard)
+
+#Subtraction
+mouse = 1000
+keyboard = 5000
+print(mouse - keyboard)
+
+mouse = 1000
+keyboard = 5000
+print(keyboard - mouse)
+
+#Multiplication
+mouse = 10
+keyboard = 500
+print(mouse * keyboard)
+
+age = 30
+
+print(myName + " i am " + str(age) + " years old.")
+
+age += 3
+print(age)
+
+print("Hello World")
